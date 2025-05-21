073aac2fccc7e3961354f62269822016ffd6c5c2
Hamza Ahmed <hamzaa@infinite.com>
2025-05-21 19:43:10 +0530

    changes in pagination and search in userlist

---
+++ frontend/src/shared/pagination/Pagination.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+import "./Pagination.css"; // Add custom styling here
+
+const Pagination = ({ pageCount, onPageChange }) => {
+  return (
+    <ReactPaginate
+      breakLabel="..."
+      nextLabel="Next"
+      previousLabel="Prev"
+      onPageChange={onPageChange}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+      pageCount={pageCount}
+      containerClassName="pagination"
+      activeClassName="active"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      previousClassName="page-item"
+      previousLinkClassName="page-link"
+      nextClassName="page-item"
+      nextLinkClassName="page-link"
+      breakClassName="page-item"
+      breakLinkClassName="page-link"
+    />
+  );
+};
+
+export default Pagination;
