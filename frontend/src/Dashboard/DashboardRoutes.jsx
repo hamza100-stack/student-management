b64d3da8a76e5d15635050d84690b7c38a221d32
HiteshVerma44 <Hitesh.verma@infinite.com>
2025-06-03 13:00:59 +0530

    starting of the courses

--- frontend/src/Dashboard/DashboardRoutes.jsx
+++ frontend/src/Dashboard/DashboardRoutes.jsx
@@ -4,6 +4,7 @@ import DashboardComponent from "./components/DashboardComponent";
 import AdminDashboard from "./components/adminDashboard/AdminDashboard";
 import UsersList from "./components/users/UsersList";
 import PageNotFound from "../shared/PageNotFound";
+import AddCourse from "./components/courses/addCourses/AddCourse";
 
 const DashboardRoutes = () => {
     return (
@@ -11,6 +12,7 @@ const DashboardRoutes = () => {
             <Route path="" element={<DashboardComponent />} />
             <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/admin/users-list" element={<UsersList />} />
+            <Route path="/admin/add-course" element={<AddCourse />} />
             <Route path="*" element={<PageNotFound />} />
 
         </Routes>
